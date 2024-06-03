27d7caf4ccfbab377a118ef70e5d4504cc38c393
gabriel <marcelo.padilla.rodriguez@udla.edu.ec>
2024-06-03 08:21:10 -0500

    creacion metodo RecargarOnRecargarClickedGP

--- EvaluacionP2GP/MainPage.xaml.cs
+++ EvaluacionP2GP/MainPage.xaml.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
 
-
 namespace EvaluacionP2GP
 {
     public partial class MainPage : ContentPage
@@ -25,12 +24,30 @@ namespace EvaluacionP2GP
                     _selectedAmount = radioButton.Value.ToString();
                     if (lbSeleccionGP != null)
                     {
-                        lbSeleccionGP.Text = $"Ha seleccionado una recarga de: ${_selectedAmount} USD";
+                        lbSeleccionGP.Text = $"Ha seleccionado una recarga de: ${_selectedAmount} dólares";
                     }
                 }
             }
         }
 
-    }
+        private async void OnRecargarClickedGP(object sender, EventArgs e)
+        {
+            if (txtNumeroTelefonicoGP != null)
+            {
+                bool confirm = await DisplayAlert("Confirmación", $"¿Desea recargar ${_selectedAmount} en el número {txtNumeroTelefonicoGP.Text}?", "Sí", "No");
+                if (confirm)
+                {
+                    string fileName = $"{txtNumeroTelefonicoGP.Text}.txt";
+                    string content = $"Se hizo una recarga de ${_selectedAmount} dólares en la siguiente fecha; {DateTime.Now:dd/MM/yyyy}";
 
+                    string filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+                    await File.WriteAllTextAsync(filePath, content);
+
+                    await DisplayAlert("Éxito", "Recarga realizada con éxito", "OK");
+                }
+            }
+        }
+
+
+    }
 }
