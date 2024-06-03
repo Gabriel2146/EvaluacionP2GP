cf1f7ff96b1376e979a21bc1f9db168605ce2807
gabriel <marcelo.padilla.rodriguez@udla.edu.ec>
2024-06-03 07:43:48 -0500

    se agrego los usings en el archivo .xaml.cs para su uso, por ahora using Microsoft.Maui.Controls; da error

--- EvaluacionP2GP/MainPage.xaml.cs
+++ EvaluacionP2GP/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace EvaluacionP2GP
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+
+namespace EvaluacionP2GP
 {
     public partial class MainPage : ContentPage
     {
