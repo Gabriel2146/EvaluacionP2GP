46547b5a10f5055b664c5f860b29214295aee0e0
gabriel <marcelo.padilla.rodriguez@udla.edu.ec>
2024-06-03 08:19:15 -0500

    creacion de metodo OnCheckedChangedGP

--- EvaluacionP2GP/MainPage.xaml.cs
+++ EvaluacionP2GP/MainPage.xaml.cs
@@ -8,13 +8,28 @@ namespace EvaluacionP2GP
 {
     public partial class MainPage : ContentPage
     {
-        private string _selectedAmount = 5;
+        private string _selectedAmount = "5";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void OnCheckedChangedGP(object sender, CheckedChangedEventArgs e)
+        {
+            if (e.Value)
+            {
+                var radioButton = sender as RadioButton;
+                if (radioButton != null)
+                {
+                    _selectedAmount = radioButton.Value.ToString();
+                    if (lbSeleccionGP != null)
+                    {
+                        lbSeleccionGP.Text = $"Ha seleccionado una recarga de: ${_selectedAmount} USD";
+                    }
+                }
+            }
+        }
 
     }
 
